4532813f81d50823781a6a9a5969e2ae55e79c29
Neboola <r.v.bykova@mail.ru>
2015-11-05 03:00:57 +0700

    05 11 15

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Телефонная книга");
         primaryStage.setScene(new Scene(root, 300, 400));
         primaryStage.show();
     }
